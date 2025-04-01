8452f2419eead034f1650267ec996911f849956a
mohith140 <118293990+mohith140@users.noreply.github.com>
2025-04-01 18:40:37 +0530

    Update serverUrl.js

--- Fundify-Frontend-main/src/constant/serverUrl.js
+++ Fundify-Frontend-main/src/constant/serverUrl.js
@@ -1,2 +1,2 @@
 export const SERVER_URL1 = 'https://fundify.onrender.com';
-export const SERVER_URL = 'https://crowd-deploy-git-main-mohiths-projects-75fccba8.vercel.app/';
+export const SERVER_URL = 'https://crowd-deploy-five.vercel.app/';
